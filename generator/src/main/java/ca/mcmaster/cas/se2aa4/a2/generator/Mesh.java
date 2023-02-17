032f08a9402326c59b78fbd6185ac234ef734935
Mankaran Rooprai <mankaranrooprai@gmail.com>
2023-02-17 13:09:51 -0500

    latet

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/Mesh.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/Mesh.java
@@ -8,17 +8,22 @@ import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
 
-
-public class Mesh  {
+public class Mesh {
     private int width = 500;
     private int height = 500;
     private int square_size = 20;
 
     List<Vertex> vertices = new ArrayList<>();
-    List<Segment> segments = new ArrayList<>();
-    int count = 0;
+    List<Vertex> verticesColored = new ArrayList<>();
 
+    List<Segment> segments = new ArrayList<>();
+    List<Segment> segmentsColored = new ArrayList<>();
+
+    HashMap<Vertex, Vertex> xy = new HashMap<>();
+
+    int count = 0;
 
     public double getX(Vertex v) {
         double valuex = v.getX();
@@ -30,13 +35,13 @@ public class Mesh  {
         return valuey;
     }
 
-    public int getV1Idx(Segment s) {
-        return s.getV1Idx();
-    }
+    // public int getV1Idx(Segment s) {
+    // return s.getV1Idx();
+    // }
 
-    public int getV2Idx(Segment s) {
-        return s.getV2Idx();
-    }
+    // public int getV2Idx(Segment s) {
+    // return s.getV2Idx();
+    // }
 
     public void setX(Vertex v, double x) {
         v.newBuilderForType().setY(x);
@@ -58,15 +63,16 @@ public class Mesh  {
         return p.getValue();
     }
 
-    public Vertex createVertex(int x, int y) {
-
-        Vertex v = Vertex.newBuilder().setX((double) x).setY((double) y).build();
-        return v;
+    public void createVertex(int x, int y) {
+        if (!vertices.contains(Vertex.newBuilder().setX((double) x).setY((double) y).build())) {
+            vertices.add(Vertex.newBuilder().setX((double) x).setY((double) y).build());
+        }
     }
 
-    public Segment createSegment(int vertindex1, int vertindex2) {
-        Segment s = Segment.newBuilder().setV1Idx(vertindex1).setV2Idx(vertindex2).build();
-        return s;
+    public void createSegment(int vertindex1, int vertindex2) {
+        if (!segments.contains(Segment.newBuilder().setV1Idx(vertindex1).setV2Idx(vertindex2).build())) {
+            segments.add(Segment.newBuilder().setV1Idx(vertindex1).setV2Idx(vertindex2).build());
+        }
     }
 
     public Property createProperty(String colorCode) {
@@ -74,18 +80,28 @@ public class Mesh  {
         return p;
     }
 
-    public Vertex createVertexColor(Vertex v, Property color) {
-        Vertex vc = Vertex.newBuilder(v).addProperties(color).build();
-        return vc;
+    public void createVertexColor(Vertex v, String colorCode) {
+        verticesColored.add(Vertex.newBuilder(v)
+                .addProperties(Property.newBuilder().setKey("rgb_color").setValue(colorCode).build()).build());
     }
 
-    public Segment createSegmentColor(Segment s, Property color) {
-        Segment sc = Segment.newBuilder(s).addProperties(color).build();
-        return sc;
+    public void createSegmentColor(Segment s, String colorCode) {
+        segmentsColored.add(Segment.newBuilder(s)
+                .addProperties(Property.newBuilder().setKey("rgb_color").setValue(colorCode).build()).build());
+    }
+
+    public void createSegments() {
+        for (int i = 0; i < verticesColored.size() - 2; i++) {
+            int temp = 0;
+            segments.add(Segment.newBuilder().setV1Idx(temp).setV2Idx(temp + i).build());
+            segments.add(Segment.newBuilder().setV1Idx(temp).setV2Idx(temp + i).build());
+        }
     }
 
     public Structs.Mesh generate(List<Vertex> verticesWithColors, List<Segment> segmentsWithColors) {
-        Structs.Mesh mesh = Structs.Mesh.newBuilder().addAllVertices(verticesWithColors).addAllSegments(segments).addAllSegments(segmentsWithColors).build();
+        Structs.Mesh mesh = Structs.Mesh.newBuilder().addAllVertices(verticesWithColors).addAllSegments(segments)
+                .addAllSegments(segmentsWithColors).build();
         return mesh;
     }
+
 }
