ffc2e6bf1db8892c19147acd4bc9117aa109a47b
AmaanK2 <khakianiamaan@gmail.com>
2023-03-15 17:14:51 -0400

    island subproject configuration

---
+++ island/src/main/java/ca.mcmaster.cas.se2aa4.a3.island/Main.java
@@ -0,0 +1,16 @@
+package ca.mcmaster.cas.se2aa4.a3.island;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+//        Configuration config = new Configuration(args);
+//        Buildable specification = SpecificationFactory.create(config);
+//        Mesh theMesh = specification.build();
+//        Structs.Mesh exported = new Exporter().run(theMesh);
+//        if(config.export().containsKey(Configuration.DEMO)) {
+//            exported = new RandomEnricher(1).process(exported);
+//        }
+//        new MeshFactory().write(exported, config.export(Configuration.FILENAME));
+    }
+}
