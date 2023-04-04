169f64c4c72d99626c34b5ef7aaf1d6daf2f5377
Mankaran Rooprai <mankaranrooprai@gmail.com>
2023-04-04 18:54:31 -0400

    Added pathfinder library as a dependency of the generator subproject

---
+++ island/src/main/java/ca.mcmaster.cas.se2aa4.a3.island/city/City.java
@@ -0,0 +1,9 @@
+package ca.mcmaster.cas.se2aa4.a3.island.city;
+
+import ca.mcmaster.cas.se2aa4.a4.pathfinder.adt.Graph;
+
+public class City {
+    public City() {
+        Graph g = new Graph();
+    }
+}
