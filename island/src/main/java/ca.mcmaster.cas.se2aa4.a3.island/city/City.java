a167d1922628633467cd00b3668dc338b8eb61b0
Mankaran Rooprai <mankaranrooprai@gmail.com>
2023-04-04 20:34:40 -0400

    Randomly selects one of the created cities as a capital (hub).

--- island/src/main/java/ca.mcmaster.cas.se2aa4.a3.island/city/City.java
+++ island/src/main/java/ca.mcmaster.cas.se2aa4.a3.island/city/City.java
@@ -3,12 +3,19 @@ package ca.mcmaster.cas.se2aa4.a3.island.city;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 import ca.mcmaster.cas.se2aa4.a3.island.terrain.Tile;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class City {
     List<Structs.Polygon> polylist;
     List<Tile> tileList;
     int numCities;
+
+    boolean isCapital = false;
+
+    static List<City> cityList = new ArrayList<>();
+
     public City(List<Structs.Polygon> polyList, List<Tile> tileList, int numCities) {
         this.polylist = polyList;
         this.tileList = tileList;
@@ -25,11 +32,19 @@ public class City {
             }
             tileIdx++;
         }
+        setCapital();
         return this.tileList;
     }
 
     private void setCityTile(Tile tile) {
         tile.createCity();
+        this.cityList.add(this);
+    }
+
+    private void setCapital() {
+        Random rand = new Random();
+        int index = rand.nextInt(this.cityList.size());
+        this.cityList.get(index).isCapital = true;
     }
 
 }
